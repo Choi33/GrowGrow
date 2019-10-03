a31a58bfb4d701b40cb300817f6d330903ff820c
seoyl98 <seoyla98@gmail.com>
2019-10-03 23:36:19 +0900

    아이콘수정및 게이지추가

---
+++ growgame/Assets/GrowGageUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrowGageUI : MonoBehaviour {
+    GameObject GrowGage;
+	// Use this for initialization
+	void Start () {
+        this.GrowGage = GameObject.Find("Growgage");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.UpArrow))
+        {
+           this.GrowGage.GetComponent<Image>().fillAmount += 0.01f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.GrowGage.GetComponent<Image>().fillAmount -= 0.01f;
+        }
+    }
+}
