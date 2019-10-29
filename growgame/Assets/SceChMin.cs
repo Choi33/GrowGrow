cde343dd3c4e451d86c4aa840bcbed6d1234d45b
Choi33 <rena258@naver.com>
2019-10-29 15:21:38 +0900

    메인에서 미니게임으로 씬 전환 추가

---
+++ growgame/Assets/SceChMin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class SceChMin : MonoBehaviour {
+
+    GameObject Loding;
+    // Use this for initialization
+
+    public void OnClick()
+    {
+        GetComponent<AudioSource>().Play();
+        Loding = GameObject.Find("LodingFade");
+        Loding.transform.Translate(-1200, 0, 0);
+        StartCoroutine(NextScene());
+    }
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("MinGame");
+        // Destroy(this.gameObject);
+    }
+}
