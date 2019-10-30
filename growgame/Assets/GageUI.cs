e51a1eaba704ecebec23776987a77287348a5416
seoyl98 <seoyla98@gmail.com>
2019-10-30 23:11:48 +0900

    아이콘 및 토양이 수정

---
+++ growgame/Assets/GageUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GageUI : MonoBehaviour {
+
+    GameObject Sungage;
+    GameObject Watergage;
+	// Use this for initialization
+	void Start () {
+        this.Sungage = GameObject.Find("Sungage");
+        this.Watergage = GameObject.Find("Watergage");
+    }
+	void Update()
+    {
+        this.Sungage.GetComponent<Image>().fillAmount = MinGetPoint.sunpoint / 50f;
+        this.Watergage.GetComponent<Image>().fillAmount = (GetPoint.waterpoint-50) / 100f;
+    }
+}
