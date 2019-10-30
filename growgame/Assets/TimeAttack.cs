a2edccc5cff1871944277dd2da8ab5e7ebebde48
whe0107 <whe0107@naver.com>
2019-10-30 16:40:25 +0900

    타임어택 구현

---
+++ growgame/Assets/TimeAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class TimeAttack : MonoBehaviour {
+
+    public float LimitTime;
+    public Text text_Timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LimitTime -= Time.deltaTime;
+        text_Timer.text = "시간 : " + Mathf.Round(LimitTime);
+
+        if(LimitTime<=0)
+        {
+            SceneManager.LoadScene("Main");
+        }
+	}
+}
